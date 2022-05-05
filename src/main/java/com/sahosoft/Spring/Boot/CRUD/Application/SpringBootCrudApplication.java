ff89d4daa21d0776563bfccfdeebabf7b478b0c3
Swagat Mishra <swagatmishra@Swagats-MacBook-Pro.local>
2022-05-05 14:30:40 +0530

    file updated

--- src/main/java/com/sahosoft/Spring/Boot/CRUD/Application/SpringBootCrudApplication.java
+++ src/main/java/com/sahosoft/Spring/Boot/CRUD/Application/SpringBootCrudApplication.java
@@ -2,6 +2,7 @@ package com.sahosoft.Spring.Boot.CRUD.Application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SpringBootCrudApplication {
--- src/main/java/controller/OrderController.java
+++ src/main/java/controller/OrderController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
+@CrossOrigin(origins = "*")
 public class OrderController {
 
     @Autowired
